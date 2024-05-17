1b309852c309504021e069218bed7964eaea797b
Justo Valentin Oberto <96108@sistemas.utn.frc.edu.ar>
2024-05-17 16:52:13 -0300

    clase usuario

---
+++ services/usuario.js
@@ -0,0 +1,11 @@
+//clase vino
+class Vino {
+    constructor(nombre, password) {
+        this.nombre = nombre;
+        this.password = password;
+    }
+    //metodo get 
+    getNombre() {
+        return this.nombre;
+    }
+}
