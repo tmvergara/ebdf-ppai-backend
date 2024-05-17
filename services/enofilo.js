a5e396f9344fa9e7ca8c8dadb490febcebe1de27
ldalmagro1 <94366@sistemas.frc.utn.edu.ar>
2024-05-17 17:01:15 -0300

    Clase enofilo y sus funciones

---
+++ services/enofilo.js
@@ -0,0 +1,28 @@
+import Usuario from './usuario.js';
+import Siguiendo from './siguiendo.js';
+
+class Enofilo {
+    // Constructor de la clase
+    constructor() {
+        this.apellido = apellido,
+            this.nombre = nombre,
+            this.imagenPerfil = imagenPerfil,
+            this.usuario = usuario;
+    }
+
+    // Método para obtener el nombre del usuario
+    getNombreUsuario() {
+        return Usuario.getNombre();
+    }
+
+    // Método para verificar si el usuario sigue a una bodega
+    sigueABodega(bodegas) {
+        for (let i = 0; i < bodegas.length; i++) {
+            if (Siguiendo.sosDeBodega(bodegas[i])) {
+                return true;
+            }
+            return false;
+        }
+    }
+
+}
