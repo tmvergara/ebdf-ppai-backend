c35d9482a579508520e862323c81c389da05ecc7
Tomas Vergara <tominacho@hotmail.com>
2024-05-18 19:30:45 -0300

    no se

--- controllers/importarActualizacionVB.controller.js
+++ controllers/importarActualizacionVB.controller.js
@@ -1,5 +1,7 @@
 const bodegas = require("../database/bodegas.json");
+const vinos = require("../database/vinos.json");
 const Bodega = require("../services/bodega");
+const Vino = require("../services/vino");
 
 class ControladorImportarActualizacionVB {
   constructor() {
@@ -16,7 +18,33 @@ class ControladorImportarActualizacionVB {
       );
     });
 
-    console.log(this.bodegas);
+    this.vinos = vinos.map((vino) => {
+      function buscarBodega(nombreBodega, bodegas) {
+        const bodega = bodegas.find((bodega) => {
+          if (bodega.getNombre() == nombreBodega) {
+            return true;
+          }
+        });
+        if (bodega !== undefined) {
+          return bodega;
+        }
+        console.log("La bodega no existe...");
+        return null;
+      }
+
+      return new Vino(
+        vino.añada,
+        vino.imgEtiqueta,
+        buscarBodega(vino.bodega, this.bodegas),
+        vino.nombre,
+        vino.notaDeCata,
+        vino.precio,
+        vino.varietal,
+        vino.maridaje
+      );
+    });
+
+    console.log(this.vinos);
   }
 
   // Operaciones/Metodos Privados
@@ -30,12 +58,30 @@ class ControladorImportarActualizacionVB {
     return this.bodegasConActualizacion;
   }
 
+  #verificarSeleccionUnica() {
+    if (this.bodegasSeleccionadas.length == 1) {
+      return true;
+    }
+    return false;
+  }
+
+  #actualizarDatosBodega() {}
+
   // Operaciones/Metodos Publicas
   opcionImportarActualizacionVentana(req, res) {
     res.status(200).json(this.#buscarBodegasConActualizacion());
   }
 
-  tomarBodegasSeleccionadas() {}
+  tomarBodegasSeleccionadas(req, res) {
+    this.bodegasSeleccionadas = req.body.bodegasSeleccionadas;
+    if (this.#verificarSeleccionUnica()) {
+      this.#actualizarDatosBodega();
+    } else {
+      res.status(500).json({
+        error: "Este flujo del caso de uso no esta implementado.",
+      });
+    }
+  }
 }
 
 module.exports = ControladorImportarActualizacionVB;
